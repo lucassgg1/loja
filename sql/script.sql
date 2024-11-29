8c82b8f69b83f5ac8dfb90a2f9a453f583767df2
Inácio <149955804+inacioads23@users.noreply.github.com>
2024-11-29 16:37:51 -0300

    Update script.sql

--- sql/script.sql
+++ sql/script.sql
@@ -1,48 +1,50 @@
--- postgres
-CREATE TABLE usuario
-(   id bigserial NOT NULL,
-    nome varchar(255),    
-    cpf varchar(30),
-    dataNascimento date,
-    email varchar(50),
-    login varchar(50),   
-    password varchar(50),
-    ativo boolean,
-    telefone varchar(15),
-    endereco varchar(255),
-    numEndereco varchar(5),
-    complEndereco varchar(50),
-    bairro varchar(50),
-    cidade varchar(100),
-    estado varchar(50),
-    uf varchar(2),
-    cep varchar(9),
-    CONSTRAINT usuario_pkey PRIMARY KEY (id)
-);
-
-CREATE TABLE papel
+CREATE TABLE model_login
 (
-    id bigserial NOT NULL,
-    tipo_papel varchar(50),
-    CONSTRAINT papel_pkey PRIMARY KEY (id)
-);
+    id integer NOT NULL DEFAULT nextval('model_login_id_seq'::regclass),
+    nome character varying(200) NOT NULL,
+    email character varying(150) NOT NULL,
+    login character varying(50) NOT NULL,
+    senha character varying(100) NOT NULL,
+    useradmin boolean NOT NULL DEFAULT false,
+    user_id bigint NOT NULL,
+    perfil character varying(50),
+    sexo character varying(20),
+    fotouser text,
+    extensaofotouser character varying(10),
+    cep character varying(12),
+    logradouro character varying(200),
+    bairro character varying(150),
+    localidade character varying(150),
+    uf character varying(3),
+    numero character varying(6),
+    complemento character varying(100),
+    datanascimento date,
+    cpf character varying(14),
+    telefone character varying(15),
+    CONSTRAINT model_login_pkey PRIMARY KEY (id),
+    CONSTRAINT model_login_login_key UNIQUE (login),
+    CONSTRAINT usuario_fk FOREIGN KEY (user_id)
+        REFERENCES model_login (id) 
+)
 
 
 
--- esta será a table INTERMEDIÁRIA entre as tables "usuario" e "papel" 
-CREATE TABLE usuario_papel
+CREATE TABLE orcamento
 (
-    usuario_id bigint NOT NULL,
-    papel_id bigint NOT NULL,	  
-    CONSTRAINT fk_usuario_papel_papel_id FOREIGN KEY (papel_id)
-        REFERENCES papel (id) ON DELETE CASCADE,
-    CONSTRAINT fk_usuario_papel_usuario_id FOREIGN KEY (usuario_id)
-        REFERENCES usuario (id) ON DELETE CASCADE 
+    id integer PRIMARY KEY NOT NULL DEFAULT nextval('orcamento_id_seq'::regclass),
+    nome character varying(200) NOT NULL,
+    email character varying(150) NOT NULL,    
+    telefone character varying(15),
+    orcamento text
+)
+
+CREATE TABLE produto
+(
+	id integer PRIMARY KEY NOT NULL DEFAULT nextval('produto_id_seq'::regclass),
+    categoria character varying(20) NOT NULL,
+    produto character varying(200) UNIQUE NOT NULL,
+    tipo character varying(20) NOT NULL,    
+    vlcompra numeric(10,2),
+    icms numeric(4,2),
+    vlvenda numeric(10,2)
 );
-
-
--- primeiro adicona as tables: "papel" e "usuario_papel"
--- depois seleciona as linhas de baixo  e executa
-INSERT INTO papel (tipo_papel) VALUES ('ADMIN');
-INSERT INTO papel (tipo_papel) VALUES ('USER');
-INSERT INTO papel (tipo_papel) VALUES ('VENDOR');
